da23ee96a4f8d5a4efb5ff2371a9ac762c815ef9
Valentin Gagarin <valentin@gagarin.work>
2024-07-03 10:18:07 +0200

    simplify build
    the `static` directory may be obsolete now, but it's not clear whether
    the videos and other things in there are still used somewhere else

--- flake-parts/website.nix
+++ flake-parts/website.nix
@@ -10,25 +10,17 @@
     }:
     let
       build =
-        baseUrl:
         pkgs.runCommand "website" { buildInputs = [ ]; } ''
           mkdir -p $out
-          cp -r ${self}/* .
-          chmod -R u+w .
-
-          cp -r website/* website/.* $out
-          rm $out/static
-          cp -r static/ $out/static
+          ls -lar ${self}
+          cp -r ${self}/website/* $out
         '';
     in
     {
-      packages.default = self'.packages.website;
-      packages.website = build "https://clan.lol";
-      packages.website-localhost = build "http://localhost:1111";
+      packages.default = build;
       packages.serve = pkgs.writeShellScriptBin "serve-local" ''
-        echo "serving: ${self'.packages.website-localhost}"
         ${pkgs.python3}/bin/python -m http.server 1111 \
-          -d ${self'.packages.website-localhost}
+          -d website
       '';
     };
 }
